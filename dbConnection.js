7fbd622389352a6e04e8157add16dad8240beafd
Aashutosh soni <ashutoshsoni130@gmail.com>
2024-12-21 12:47:04 +0530

    update: removed extra verbosity

--- dbConnection.js
+++ dbConnection.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql2/promise");
 
-// Create a connection pool  
+// Create a db connection pool  
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    debug: true,
+    debug: false,
 });
 
-// Test database connection
+// Test db connection
 const testConnection = async () => {
     try {
         const conn = await pool.getConnection();
